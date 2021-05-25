6c4acc621683c829d5c4754d4d151213a5faf0e5
Andy Nguyen <Ydna317@gmail.com>
2021-05-24 21:30:04 -0700

    routing

--- client/src/components/SpotifyPlayer/config.js
+++ client/src/components/SpotifyPlayer/config.js
@@ -2,7 +2,8 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "7737cf22222c4dc4a8ac5c5439927cc9";
-export const redirectUri = "http://localhost:3000/chatroom";
+export const redirectUri = "/chatroom";
+// export const redirectUri = "http://localhost:3000/chatroom";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
