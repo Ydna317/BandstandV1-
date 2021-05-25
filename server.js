8be6bad16bfc9b026a240fed47f68330a6c6f535
Andy Nguyen <Ydna317@gmail.com>
2021-05-24 21:20:44 -0700

    adding path

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ const io = socketIo(server, {
   },
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 4000;
 const USER_JOIN_CHAT_EVENT = "USER_JOIN_CHAT_EVENT";
 const USER_LEAVE_CHAT_EVENT = "USER_LEAVE_CHAT_EVENT";
