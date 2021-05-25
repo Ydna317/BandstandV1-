06189fea8ea6cb289e7c40cc7f06d6ffd153c62d
Andy Nguyen <Ydna317@gmail.com>
2021-05-25 09:27:14 -0700

    jfc

--- client/src/useChat.js
+++ client/src/useChat.js
@@ -9,9 +9,9 @@ const STOP_TYPING_MESSAGE_EVENT = "STOP_TYPING_MESSAGE_EVENT";
 
 
 // LOOK HERE.
-const SOCKET_SERVER_URL = "http://localhost:4000";
+// const SOCKET_SERVER_URL = "http://localhost:4000";
 // const SOCKET_SERVER_URL = "http://bandstandv1.herokuapp.com/socket.io/?EIO=4&transport=websocket";
-// const SOCKET_SERVER_URL = "https://bandstandv1.herokuapp.com";
+const SOCKET_SERVER_URL = "https://bandstandv1.herokuapp.com/chatroom";
 
 
 const useChat = (roomId) => {
